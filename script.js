4fee29c146390b15e40a2e3e53c1e632b609fc40
Julien de Fondaumière <julien.de-fondaumiere@etu.umontpellier.fr>
2017-11-30 16:21:05 +0100

    des trucs

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function(){
+
+	console.log("Ready !");
+	$("#button").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#debut").offset().top
+	    }, 800);
+	});
+});
