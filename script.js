e3008d8abc05c57246e27c7379a821a4ac7ba7ef
Julien de Fondaumière <julien.de-fondaumiere@etu.umontpellier.fr>
2017-11-30 18:02:07 +0100

    C BON

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 ﻿$(document).ready(function(){
 
 	console.log("Ready !");
+
+	$.getJSON("listeImg.json", function(data) {
+
+		$.each(data["images"], function(i) {
+
+			$("#listImg").append("<li><a href='#'><img src='" + data["images"][i].path + "'/></a></li>");
+		});
+	});
+
 	$("#button").click(function() {
 	    $('html, body').animate({
 	        scrollTop: $("#debut").offset().top
 	    }, 800);
 	});
+
 });
+
